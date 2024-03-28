802d7e96eec1b7427a800631e57a784219694a48
unknown <carlos2000sistemasc@gmail.com>
2024-03-27 23:00:14 -0700

    Primer commit del proyecto

---
+++ src/components/Navegacion.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+<template>
+<header class="cabeza">
+    <div class="cabeza__logo">B LIFE</div>
+
+    <div class="cabeza__navegacion">
+        <nav class="navegacion">
+            <RouterLink to="/">PRODUCTO</RouterLink>
+            <RouterLink to="/">PROMOCIONES</RouterLink>
+            <RouterLink to="/">NOSOTROS</RouterLink>
+        </nav>
+    </div>
+
+
+    <div class="cabeza__iconos">
+        <nav>
+            <RouterLink to="/">PRODUCTO</RouterLink>
+            <RouterLink to="/">PROMOCIONES</RouterLink>
+            <RouterLink to="/">NOSOTROS</RouterLink>
+        </nav>
+    </div>
+    
+</header>
+    
+</template>
+
+<style scoped>
+.cabeza{
+    background: red;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.cabeza__navegacion{
+    background: greenyellow;
+    width: 40%;
+}
+.navegacion{
+    background: olive;
+    display: flex;
+    justify-content: space-between;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navegacion from '@/components/Navegacion.vue';
+</script>
+
+<template>
+
+  <Navegacion/>
+<h1>
+  Hola mundo
+</h1>
+</template>
